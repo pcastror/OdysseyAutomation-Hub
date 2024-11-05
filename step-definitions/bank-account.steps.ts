725e8e05c7c0104c99f7c214194fa4dd43a59201
paulascr <162180431+paulascr@users.noreply.github.com>
2024-11-05 17:46:36 -0300

    attempt5

--- step-definitions/bank-account.steps.ts
+++ step-definitions/bank-account.steps.ts
@@ -1,23 +1,34 @@
-import { binding, given, then, when} from 'cucumber-tsflow';
-import { assert } from 'chai';
+import {binding, given, then, when} from 'cucumber-tsflow';
+import {assert} from 'chai';
 import {setDefaultTimeout} from "@cucumber/cucumber";
+import * as url from "url";
 
 setDefaultTimeout(60 * 1000);
+
 @binding()
 export class BankAccountSteps {
-  private accountBalance: number = 0;
+    private accountBalance: number = 0;
 
-  @given("A bank account with starting balance of {string}")
-  public givenAnAccountWithStartingBalance(amount: string) {
-    this.accountBalance = parseInt(amount);
-  }
-  @when("{string} is deposited")
-  public deposit(amount: string) {
-    this.accountBalance = Number(this.accountBalance) + parseInt(amount);
-  }
-  @then("The bank account balance should be {string}")
-  public async accountBalanceShouldEqual(expectedAmount: string) {
-    assert.equal(this.accountBalance, parseInt(expectedAmount));
+    @given("A bank account with starting balance of {string}")
+    public givenAnAccountWithStartingBalance(amount: string) {
+        this.accountBalance = parseInt(amount);
+    }
 
-  }
+    @when("{string} is deposited")
+    public deposit(amount: string) {
+        this.accountBalance = Number(this.accountBalance) + parseInt(amount);
+    }
+
+    @then("The bank account balance should be {string}")
+    public async accountBalanceShouldEqual(expectedAmount: string) {
+        assert.equal(this.accountBalance, parseInt(expectedAmount));
+        let UrlPath = process.env.URL_PATH;
+
+        if (UrlPath == "actions_test") {
+            console.log("Variable rescatada con éxito")
+        } else {
+            console.log("Si ves este mensaje es porque fallamos")
+        }
+
+    }
 }
