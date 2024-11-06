4f4c633272a3abb3d4e814923fe2803997eb413e
paulascr <162180431+paulascr@users.noreply.github.com>
2024-11-06 13:18:50 -0300

    First test axios

---
+++ clients/client-credentials.service.ts
@@ -0,0 +1,13 @@
+export class ClientCredentialsService {
+    private url: string
+
+
+    constructor() {
+
+        this.url = process.env.URL ?? "https://swapi.dev";
+    }
+
+    getUrl(): string {
+        return this.url;
+    }
+}
---
+++ features/swapiPeople-get.feature
@@ -0,0 +1,7 @@
+Feature: Example 1 to actions
+  In order to know Starwars info
+
+  Scenario: starwars characters
+    Given A Character id 1
+    When get swapi is call
+    Then return "Luke Skywalker" in name field
