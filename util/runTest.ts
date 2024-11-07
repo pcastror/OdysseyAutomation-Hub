46f1c4ef63fde7a8fd535ae57437e3cb34412ed3
paulascr <162180431+paulascr@users.noreply.github.com>
2024-11-07 10:51:31 -0300

    Clean code

--- util/runTest.ts
+++ util/runTest.ts
@@ -1,34 +1,29 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
 import fs from 'fs';
 
-// Función para eliminar secuencias ANSI
 function removeAnsiCodes(text: string): string {
     return text.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, '');
 }
+
 exec('npm run test:cucumber', (error, stdout, stderr) => {
-    // Combina stdout y stderr para capturar toda la salida
-    const output = stdout + stderr;
-
-    // Elimina secuencias ANSI antes de guardar
-    const cleanOutput = removeAnsiCodes(output);
-
-    // Guardar toda la salida limpia en un archivo
-    fs.writeFileSync('cucumber-console-output.txt', cleanOutput, 'utf-8');
-    //console.log("Los resultados completos, sin secuencias ANSI, se han guardado en 'cucumber-console-output.txt'");
 
     if (error) {
         console.error(`Error ejecutando Cucumber: ${error}`);
+    } else {
+        const output = stdout + stderr;
+        const cleanOutput = removeAnsiCodes(output);
+        fs.writeFileSync('cucumber-console-output.txt', cleanOutput, 'utf-8');
+        const urlRegex = /https:\/\/reports\.cucumber\.io\/reports\/[a-f0-9-]+/g;
+        const foundUrls = cleanOutput.match(urlRegex);
+
+        let reportUrl = '';
+
+        if (foundUrls && foundUrls.length > 0) {
+            reportUrl = foundUrls[0]; // Tomamos la primera URL que coincide
+            process.env.CUCUMBER_REPORT_URL = reportUrl;
+            console.log(stdout);
+            console.log(stderr);
+            //console.log(reportUrl);
+        }
     }
-
-    const urlRegex = /https:\/\/reports\.cucumber\.io\/reports\/[a-f0-9-]+/g;
-    const foundUrls = cleanOutput.match(urlRegex);
-
-    let reportUrl = '';
-    if (foundUrls && foundUrls.length > 0) {
-        reportUrl = foundUrls[0]; // Tomamos la primera URL que coincide
-        process.env.CUCUMBER_REPORT_URL = reportUrl;
-        console.log(reportUrl);
-
-    }
-
 });
